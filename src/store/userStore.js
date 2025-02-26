d7519936c7d61ceb7931f51c54434395a8fd5f62
SargisCH <sargis.chilingaryann@gmail.com>
2025-02-26 18:12:05 +0400

    syntax issue fixed

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,14 +1,18 @@
-import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export const useUserStore = create()(
   persist(
     (set, get) => ({
       users: [],
       add: (newUser) => set((state) => ({ users: [...state.users, newUser] })),
-      removeUser: (id) => set((state) => ({ users: state.users.filter(user => user.id !== id) })),
+      removeUser: (id) =>
+        set((state) => ({
+          users: state.users.filter((user) => user.id !== id),
+        })),
     }),
     {
-      name: 'users', 
-  ),
-)
+      name: "users",
+    }
+  )
+);
